83c109f98707779d74581671b798f1bb11ae6f10
Alperen Polat <m_alperenPolat@hotmail.com>
2022-10-16 20:45:48 +0300

    business project update

---
+++ PetApp.Business/Abstract/IPetService.cs
@@ -0,0 +1,16 @@
+﻿using PetApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetApp.Business.Abstract
+{
+    public interface IPetService
+    {
+        public List<Pet> GetAll();
+        public void Create(Pet pet);
+        public void Delete(Pet pet);
+        public void Update(Pet pet);
+        public Pet GetById(int id);
+    }
+}
---
+++ PetApp.Business/Concrete/PetManager.cs
@@ -0,0 +1,45 @@
+﻿using PetApp.Business.Abstract;
+using PetApp.Data.Abstract;
+using PetApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetApp.Business.Concrete
+{
+    public class PetManager : IPetService
+    {
+        private IPetRepository _petRepository;
+
+        public PetManager(IPetRepository petRepository)
+        {
+            this._petRepository = petRepository;
+        }
+
+        public void Create(Pet pet)
+        {
+            // iş kuralları
+            _petRepository.Create(pet);
+        }
+
+        public void Delete(Pet pet)
+        {
+            _petRepository.Delete(pet);
+        }
+
+        public List<Pet> GetAll()
+        {
+            return _petRepository.GetAll();
+        }
+
+        public Pet GetById(int id)
+        {
+            return _petRepository.GetById(id);
+        }
+
+        public void Update(Pet pet)
+        {
+            _petRepository.Update(pet);
+        }
+    }
+}
